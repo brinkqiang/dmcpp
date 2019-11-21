11d9ee652ea0e0718ad5552ca7cd884397f58eec
brinkqiang <brink.qiang@gmail.com>
2019-11-21 15:00:09 +0800

    add

--- test/dmstring/dmstring.cpp
+++ test/dmstring/dmstring.cpp
@@ -11,9 +11,25 @@ inline void append(std::string& s, Args&&... args)
     ((s += std::forward<Args>(args), s += ", "), ...);
 }
 
+//template<typename... Args>
+//void print(Args... Args){
+//    std::initializer_list<int>{(std::cout << args << std::endl, 0)...};
+//}
+
+//c++17 unary fold
+template<typename... Args>
+void print(Args... args) {
+    ((std::cout << args << std::endl), ...);
+}
+//
+////c++17 binary fold
+//template<typename ...Args>void print(Args&&... args) {
+//    (std::cout << ... << args) << '\n';
+//}
+
 int main() {
     std::string s;    
     append(s, "1", "2", "3");
-
+    print("1", "2", "3");
     return 0;
 }
