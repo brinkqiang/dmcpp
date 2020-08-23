7a32747e153687ae029ab714a0303d9d361ec3e2
brinkqiang <brink.qiang@gmail.com>
2020-08-23 23:27:49 +0800

    add

---
+++ test/dmmaptest/dmmaptest.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <mutex>
+#include <thread>
+#include <iostream>
+#include <queue>
+#include <chrono>
+
+int main()
+{
+    {
+        std::map<int, int> mapData{ {1, 2},  {3, 4},  {5, 6} };
+
+        auto it = mapData.find(1);
+
+        for (int i = 10; i < 1000; i++)
+        {
+            mapData.insert(std::make_pair(i, i));
+        }
+
+        std::cout << it->second << std::endl;
+    }
+    {
+        std::unordered_map<int, int> mapData{ {1, 2},  {3, 4},  {5, 6} };
+
+        auto it = mapData.find(1);
+
+        for (int i = 10; i < 1000; i++)
+        {
+            mapData.insert(std::make_pair(i, i));
+        }
+
+        std::cout << it->second << std::endl;
+    }
+
+    return 0;
+}
