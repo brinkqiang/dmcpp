c5bc7efa63452dcb5cc432d63ff8b58a0ce735bf
brinkqiang <brink.qiang@gmail.com>
2020-12-05 23:47:37 +0800

    add

--- test/dmtupletest/dmtupletest.cpp
+++ test/dmtupletest/dmtupletest.cpp
@@ -2,34 +2,43 @@
 #include <iostream>
 #include <tuple>
 #include <string>
+#include "dmcast.h"
 
-template<int index, typename... Ts>
+template<int index, typename... T>
 struct print_tuple {
-	void operator() (std::tuple<Ts...>& t) {
+	void operator() (std::tuple<T...>& t) {
 		std::cout << get<index>(t) << " ";
-		print_tuple<index - 1, Ts...>{}(t);
+		print_tuple<index - 1, T...>{}(t);
 	}
 };
 
-template<typename... Ts>
-struct print_tuple<0, Ts...> {
-	void operator() (std::tuple<Ts...>& t) {
+template<typename... T>
+struct print_tuple<0, T...> {
+	void operator() (std::tuple<T...>& t) {
 		std::cout << get<0>(t) << " ";
 	}
 };
 
-template<typename... Ts>
-void print(std::tuple<Ts...>& t) {
-	const auto size = std::tuple_size<std::tuple<Ts...>>::value;
-	print_tuple<size - 1, Ts...>{}(t);
+template<typename... T>
+void print(std::tuple<T...>& t) {
+	const auto size = std::tuple_size<std::tuple<T...>>::value;
+	print_tuple<size - 1, T...>{}(t);
+}
+
+template<typename... T>
+std::string csv(std::tuple<T...>& t)
+{
+	std::string strData;
+	std::apply([&strData](auto&& ... args) { ((strData += (strData.empty() ? "" : ","), strData += dmcast::lexical_cast(args)), ...); }, t);
+	return strData;
 }
 
 int main()
 {
 	auto t = std::make_tuple(1, 2, "abc", "def", 4.0f);
-	print(t);
+	auto s = csv(t);
 
-    auto [x, y, z] = std::make_tuple(1, 2.3, "456");
-    std::cout << x << ", " << y << ", " << z << std::endl;
-    return 0;
+	std::cout << s << " ";
+
+	return 0;
 }
