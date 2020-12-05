89cebc59b8896be83547e9c72dbfab4aee26fb5b
brinkqiang <brink.qiang@gmail.com>
2020-12-05 22:58:06 +0800

    add

--- test/dmtupletest/dmtupletest.cpp
+++ test/dmtupletest/dmtupletest.cpp
@@ -3,12 +3,33 @@
 #include <tuple>
 #include <string>
 
-auto f() {
-    return std::make_tuple(1, 2.3, "456");
+template<int index, typename... Ts>
+struct print_tuple {
+	void operator() (std::tuple<Ts...>& t) {
+		std::cout << get<index>(t) << " ";
+		print_tuple<index - 1, Ts...>{}(t);
+	}
+};
+
+template<typename... Ts>
+struct print_tuple<0, Ts...> {
+	void operator() (std::tuple<Ts...>& t) {
+		std::cout << get<0>(t) << " ";
+	}
+};
+
+template<typename... Ts>
+void print(std::tuple<Ts...>& t) {
+	const auto size = std::tuple_size<std::tuple<Ts...>>::value;
+	print_tuple<size - 1, Ts...>{}(t);
 }
 
-int main() {
-    auto[x, y, z] = f();
+int main()
+{
+	auto t = std::make_tuple(1, 2, "abc", "def", 4.0f);
+	print(t);
+
+    auto [x, y, z] = std::make_tuple(1, 2.3, "456");
     std::cout << x << ", " << y << ", " << z << std::endl;
     return 0;
 }
