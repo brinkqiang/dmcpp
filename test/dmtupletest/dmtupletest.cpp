386bee34c48e361ee6c337e52c910cfee3239520
brinkqiang <brink.qiang@gmail.com>
2020-12-11 16:35:44 +0800

    add

--- test/dmtupletest/dmtupletest.cpp
+++ test/dmtupletest/dmtupletest.cpp
@@ -5,17 +5,16 @@
 #include "dmcast.h"
 #include "dmformat.h"
 
-template <typename T>
+template <typename ... T>
 std::string stl_tostring(std::tuple<T...>& t)
 {
 	std::string strData;
-	std::apply([&](auto&& ... args) { ((strData += (strData.empty() ? "" : ","), strData += lexical_cast(args)), ...); }, t);
+	std::apply([&](auto&& ... args) { ((strData += (strData.empty() ? "" : ","), strData += dmcast::lexical_cast(args)), ...); }, t);
 	return strData;
 }
 
-int main()
-{
+int main(){
 	auto t = std::make_tuple(1, 2, "abc", "def", 4.0f);
-	fmt::print(stl_tostring(t));
+	fmt::printf(stl_tostring(t));
 	return 0;
 }
